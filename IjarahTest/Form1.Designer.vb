e84a5de3db704b539b3f5edaeb256d671c171477
unknown <Ron@Ron-PC.afras2.afras2.loc>
2015-05-06 12:03:04 +0300

    First Test

---
+++ IjarahTest/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonPost = New System.Windows.Forms.Button
+        Me.TextBoxRequest = New System.Windows.Forms.TextBox
+        Me.TextBoxResult = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'ButtonPost
+        '
+        Me.ButtonPost.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonPost.Location = New System.Drawing.Point(12, 422)
+        Me.ButtonPost.Name = "ButtonPost"
+        Me.ButtonPost.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonPost.TabIndex = 11
+        Me.ButtonPost.Text = "Post"
+        Me.ButtonPost.UseVisualStyleBackColor = True
+        '
+        'TextBoxRequest
+        '
+        Me.TextBoxRequest.Location = New System.Drawing.Point(12, 12)
+        Me.TextBoxRequest.MaxLength = 0
+        Me.TextBoxRequest.Multiline = True
+        Me.TextBoxRequest.Name = "TextBoxRequest"
+        Me.TextBoxRequest.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBoxRequest.Size = New System.Drawing.Size(833, 168)
+        Me.TextBoxRequest.TabIndex = 9
+        Me.TextBoxRequest.WordWrap = False
+        '
+        'TextBoxResult
+        '
+        Me.TextBoxResult.Location = New System.Drawing.Point(12, 192)
+        Me.TextBoxResult.MaxLength = 0
+        Me.TextBoxResult.Multiline = True
+        Me.TextBoxResult.Name = "TextBoxResult"
+        Me.TextBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBoxResult.Size = New System.Drawing.Size(833, 224)
+        Me.TextBoxResult.TabIndex = 10
+        Me.TextBoxResult.WordWrap = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(857, 455)
+        Me.Controls.Add(Me.ButtonPost)
+        Me.Controls.Add(Me.TextBoxRequest)
+        Me.Controls.Add(Me.TextBoxResult)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MT940 Test"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ButtonPost As System.Windows.Forms.Button
+    Friend WithEvents TextBoxRequest As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxResult As System.Windows.Forms.TextBox
+
+End Class
---
+++ IjarahTest/Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.Net
+Imports System.Text
+Imports System.IO
+Imports System.Net.Security
+Imports System.Security.Cryptography
+Imports System.Security.Cryptography.Pkcs
+Imports System.Security.Cryptography.X509Certificates
+
+Public Class Form1
+
+    Dim keyPath As String = "key.p12"
+    Dim keyPass As String = "b2buat"
+
+    Public Function AcceptAllCertifications(ByVal sender As Object, ByVal certification As System.Security.Cryptography.X509Certificates.X509Certificate, ByVal chain As System.Security.Cryptography.X509Certificates.X509Chain, ByVal sslPolicyErrors As System.Net.Security.SslPolicyErrors) As Boolean
+        Return True
+    End Function
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBoxRequest.Text = "<PaymentInfoMessage>"
+        TextBoxRequest.Text = TextBoxRequest.Text & vbCrLf & "<PaymentInfoRequest>"
+        TextBoxRequest.Text = TextBoxRequest.Text & vbCrLf & "<BankCode>RIBLSARI</BankCode>"
+        TextBoxRequest.Text = TextBoxRequest.Text & vbCrLf & "<CompanyCode>IJARAH</CompanyCode>"
+        TextBoxRequest.Text = TextBoxRequest.Text & vbCrLf & "<StartDate>" & Format(System.DateTime.Now, "yyyy-MM-dd") & "T00:00:00</StartDate>"
+        TextBoxRequest.Text = TextBoxRequest.Text & vbCrLf & "<EndDate>" & Format(System.DateTime.Now, "yyyy-MM-dd") & "T00:00:00</EndDate>"
+        TextBoxRequest.Text = TextBoxRequest.Text & vbCrLf & "<TransactionType>940</TransactionType>"
+        TextBoxRequest.Text = TextBoxRequest.Text & vbCrLf & "</PaymentInfoRequest>"
+        TextBoxRequest.Text = TextBoxRequest.Text & vbCrLf & "</PaymentInfoMessage>"
+    End Sub
+
+    Private Sub ButtonPost_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonPost.Click
+        Try
+            Dim encoding As New UTF8Encoding
+            Dim bytesToWrite As Byte() = encoding.GetBytes(TextBoxRequest.Text)
+
+            Dim url As String = "https://rbb2btest.riyadbank.com/b2b/epay2"
+            Dim request As HttpWebRequest = DirectCast(HttpWebRequest.Create(url), HttpWebRequest)
+            request.Method = "POST"
+            request.ContentLength = bytesToWrite.Length
+            request.ContentType = "text/xml; charset=utf-8"
+            ' request.ClientCertificates.Add(New X509Certificate2(@"C:\ijarahcert\key.p12", "b2buat"))
+            request.ClientCertificates.Add(New X509Certificate2("C:\ijarahcert\key.p12", "b2buat"))
+            'request.ClientCertificates.Add(New X509Certificate2(keyPath, keyPass))
+
+            ServicePointManager.ServerCertificateValidationCallback = AddressOf AcceptAllCertifications
+
+            Dim newStream As Stream = request.GetRequestStream()
+            newStream.Write(bytesToWrite, 0, bytesToWrite.Length)
+            newStream.Close()
+
+            Dim response As HttpWebResponse = DirectCast(request.GetResponse(), HttpWebResponse)
+            Dim dataStream As Stream = response.GetResponseStream()
+            Dim reader As New StreamReader(dataStream)
+            Dim responseFromServer As String = reader.ReadToEnd()
+
+            responseFromServer = Replace(responseFromServer, "><", ">" & vbCrLf & "<")
+
+            TextBoxResult.Text = responseFromServer
+        Catch ex As Exception
+            TextBoxResult.Text = ex.Message
+        End Try
+    End Sub
+
+End Class
